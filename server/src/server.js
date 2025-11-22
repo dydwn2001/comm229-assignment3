993ff9e9278634f0247b4568c9908bdc77c52047
Yongju Lee <dydwn2001@gmail.com>
2025-11-22 13:56:29 -0500

    env file is removed and config will be used.

--- server/src/server.js
+++ server/src/server.js
@@ -8,13 +8,13 @@ import qualificationCtrl from '../routes/qualifications.routes.js'
 import projectCtrl from '../routes/projects.routes.js'
 import userCtrl from '../routes/user.routes.js'
 import authCtrl from '../routes/auth.routes.js'
-
+import config from "../../config/config.js";
 
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = config.port || 3000;
 
 app.use(express.json());
 app.use(helmet());
@@ -44,7 +44,7 @@ app.get('/', (req, res) =>{
 async function connect() {
   console.log('Starting DB connection...');
   try
-  { await mongoose.connect(process.env.MONGO_URI);
+  { await mongoose.connect(config.mongoUri);
   console.log("data base successfully connected");
   app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}/`)
